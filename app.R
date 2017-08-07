706cad5b2985408eca66a71de98d833c3062a550
WeeBeasties <franklc@ferris.edu>
2017-08-06 21:24:10 -0400

    Starting to edit css code to customize the dashboard appearance

--- app.R
+++ app.R
@@ -24,9 +24,54 @@ ui <- dashboardPage(
 			      "Spring 2018" = 4))
 	),
 	dashboardBody(
+		# Setting the CSS of the dashboard to look prettier
+		tags$style(HTML("
+		body > div.wrapper > div > section > h2 {
+			display: block;
+			position: relative;
+			top: -11px;
+			font-size: 35px;
+			font-weight: 150;
+		}
+		body > div > div > section > p {
+			font-size: 18px;
+		}
+		#overallBox > div > div.icon-large {
+			top: 5px;
+			right: 20px;
+			font-size: 70px;
+			color: rgba(0, 0, 0, .20)
+		}
+		#overallBox > div > div.inner > p {
+			font-size: 18px;
+			font-weight: bold;
+		}
+		#collBox > div > div.icon-large {
+			top: 0px;
+			right: 20px;
+			font-size: 70px;
+			color: rgba(0, 0, 0, .20)
+		}
+		#collBox > div > div.inner > p {
+			font-size: 18px;
+			font-weight: bold;
+		}
+		#commBox > div > div.icon-large {
+			top: 0px;
+			right: 20px;
+			font-size: 70px;
+			color: rgba(0, 0, 0, .20)
+		}
+		#commBox > div > div.inner > p {
+			font-size: 18px;
+			font-weight: bold;
+		}
+
+		")),
+
 		img(src = 'boxflame.png', align = "left"),
 		titlePanel("  Ferris State University"),
-		p("This is a demonstration dashboard for the revised General Education program at Ferris State University. It has been created using Shiny and the R programming language. A full dashboard would contain several additional slicers in the sidebar and operational instructions with advice on interpretation. This tool is meant to provide a clean and clear overview of the program's status."),
+		p("This is a demonstration dashboard for the revised General Education program at Ferris State University. It has been created using Shiny and the R programming language. A full dashboard would contain several additional filters in the sidebar and operational instructions with advice on interpretation. This tool is meant to provide a clean and clear overview of the program's status."),
 		fluidRow(
 			valueBoxOutput("overallBox", width = 12)
 		),
